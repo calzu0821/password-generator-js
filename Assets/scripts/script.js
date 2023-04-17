0755a638c2af15b50391de4d905c2a0370015e0a
Cristina Alzuri <cristinaalzuri@gmail.com>
2023-04-16 21:02:10 -0400

    added a function and if/else statements to set the parameters for generating a password for the user

--- Assets/scripts/script.js
+++ Assets/scripts/script.js
@@ -1,12 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Password Arrays
-var upperCaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-var lowerCaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-var specialCharaters = ["!", "@", "#", "$", "%", "^", "&", "*", "-", "_", "+", "=", "?", "/"]
-
 // Write password to the #password input
 // This is the script that writes the password to the page and handles the code.
 function writePassword() {
@@ -17,12 +11,34 @@ function writePassword() {
 
 }
 
+// Password Arrays
+var upperCaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var lowerCaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var numericCharacters = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+var specialCharaters = ["!", "@", "#", "$", "%", "^", "&", "*", "-", "_", "+", "=", "?", "/"]
+
+/* Generating the user password:
+Here we use a function and an if/else statement to set the parameters for generating a password for the user. */
+function generatePassword() {
+  var passwordLength = prompt("How many characters would you like your password to contain?");
+    if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
+      alert("Please enter a valid number length bewtween 8 and 128 characters.");
+      generatePassword()
+     }
+    else {
+      lowerCase = confirm("Please click OK to to confirm including lower case characters.");
+      upperCase = confirm("Please click OK to to confirm including upper case characters.");
+      numbers = confirm ("Please click OK to to confirm including numeric characters.");
+      special = confirm("Please click OK to to confirm including special characters.");
+    }
+}
+
 // Add event listener to generate button
 // Handles the click on the password button (we havent learned how to do this yet).
 generateBtn.addEventListener("click", writePassword);
 
 
-/*
+/* Professors code from the last class on Thurs 4/13.
 
 var choices=["R","P","S"];
 
@@ -38,5 +54,4 @@ function getRandomValueFromArray(array) {
   var randomArrayPosition=getRandomNumber(0,array.length);
   return array[randomArrayPosition];
 }
-
 */
